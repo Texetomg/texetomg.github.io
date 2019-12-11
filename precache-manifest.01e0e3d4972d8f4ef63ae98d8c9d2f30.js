d29044142c325e443c0ce4ebe374fc1a3e9ce8e3
Bfalmer <texetomg@gmail.com>
2019-12-12 01:07:26 +0300

    test

---
+++ precache-manifest.01e0e3d4972d8f4ef63ae98d8c9d2f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6534d26544b15a79ae75e5ddfea774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8fb60b3837bdd2ecc56",
+    "url": "/static/css/main.846e985d.chunk.css"
+  },
+  {
+    "revision": "68652e5ea166f35af08f",
+    "url": "/static/js/2.c956f25c.chunk.js"
+  },
+  {
+    "revision": "d8fb60b3837bdd2ecc56",
+    "url": "/static/js/main.7320be77.chunk.js"
+  },
+  {
+    "revision": "68161b05809a98313def",
+    "url": "/static/js/runtime-main.e16e18b8.js"
+  },
+  {
+    "revision": "621781cd9f8e7b8f7bb632c50da6cf96",
+    "url": "/static/media/calendar.621781cd.png"
+  }
+]);
